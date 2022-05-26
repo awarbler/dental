80a8bd5eb00b569e56aea99b4c8b2fb54956e63f
awarbler <awoodfor@byui.edu>
2022-05-26 14:29:47 -0500

    create a static tag

--- dentalWeb/dentalWeb/urls.py
+++ dentalWeb/dentalWeb/urls.py
@@ -19,6 +19,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns #added to try to get static towork 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
